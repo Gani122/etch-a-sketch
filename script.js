852722c6e43b70af2bba0c596d2a53fcd8a08338
Abdi Ahmed <128744175+Gani122@users.noreply.github.com>
2023-07-30 15:10:32 -0400

    Create a 16x16 grid of divs

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('#container');
+
+for(let i = 0; i < 16; i++) {
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add('row');
+
+    for(let i = 0; i< 16; i++) {
+        const columnDiv = document.createElement('div');
+        columnDiv.classList.add('column');
+        rowDiv.appendChild(columnDiv);
+    }
+
+    container.appendChild(rowDiv);
+}
