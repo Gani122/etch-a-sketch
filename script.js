4e2cfd945411cb492083340a0a5067f8102e6100
Abdi Ahmed <128744175+Gani122@users.noreply.github.com>
2023-07-30 18:14:00 -0400

    Allow mouse hover to create red trail Create button that prompts user to specify grid size Grid size limited to 100 squares, entries above 100 set to 16

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const container = document.querySelector('#container');
 
-for(let i = 0; i < 16; i++) {
-    const rowDiv = document.createElement('div');
-    rowDiv.classList.add('row');
+function gridMaker(size = 16){
+    for(let i = 0; i < size; i++) {
+        const rowDiv = document.createElement('div');
+        rowDiv.classList.add('row');
 
-    for(let i = 0; i< 16; i++){
-        const columnDiv = document.createElement('div');
-        columnDiv.classList.add('column');
-        rowDiv.appendChild(columnDiv);
+        for(let i = 0; i< size; i++){
+            const columnDiv = document.createElement('div');
+            columnDiv.classList.add('column');
+            rowDiv.appendChild(columnDiv);
+        }
+
+        container.appendChild(rowDiv);
     }
+    const columns = document.querySelectorAll('div.column');
 
-    container.appendChild(rowDiv);
-}
-
-const columns = document.querySelectorAll('div.column');
-
-columns.forEach((column) => {
+    columns.forEach((column) => {
     column.addEventListener('mouseover', () => {
         column.style.backgroundColor = "red";
     });
-});
+    });
+}
+gridMaker();
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () =>{
+    while(container.firstChild){
+        container.removeChild(container.lastChild);
+    }
+    let size = prompt("Enter the number of boxes you want on each side of the grid (LIMIT: 100):", 16);
+    if (size > 100){
+        size = 16;
+    }
+    gridMaker(size);
+})
+
