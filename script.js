af272fb32ed220fb9c8d87ee1ea40c64e791987b
Abdi Ahmed <128744175+Gani122@users.noreply.github.com>
2023-07-30 17:22:08 -0400

    Make square 16x16 grid that scales by viewport

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ for(let i = 0; i < 16; i++) {
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row');
 
-    for(let i = 0; i< 16; i++) {
+    for(let i = 0; i< 16; i++){
         const columnDiv = document.createElement('div');
         columnDiv.classList.add('column');
         rowDiv.appendChild(columnDiv);
